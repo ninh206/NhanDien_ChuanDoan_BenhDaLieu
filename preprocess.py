1ce878e95bc793690ca2749d63b5519fd1b7190a
Dinh Ninh <vqnninh@gmail.com>
2026-02-26 16:28:08 +0700

    preprocessv2

--- preprocess.py
+++ preprocess.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+from sklearn.model_selection import train_test_split
 df = pd.read_csv('HAM10000_metadata.csv')
 df['age']=df['age'].fillna(df['age'].mean())
 
@@ -9,4 +10,6 @@ df["full_path"] = df["image_id"].apply(lambda x : os.path.join(path_img, x+".jpg
 
 df_encoded = pd.get_dummies(df,columns=["sex","localization"],dtype=int)
 df_encoded['label']=df_encoded["dx"].map({'nv':0, 'mel':1, 'bkl':2, 'bcc':3, 'akiec':4, 'vasc':5, 'df':6})
-print(df_encoded['label'])
+train_df,test_df = train_test_split(df_encoded, test_size=0.2, random_state=15, stratify=df_encoded['label'])
+train_df.to_csv('train_df.csv', index=False)
+test_df.to_csv('test_df.csv', index=False)
