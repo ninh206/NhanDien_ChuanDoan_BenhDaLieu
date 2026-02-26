bca688288f77f40c388ec42efac009651c3e5ea0
Dinh Ninh <vqnninh@gmail.com>
2026-02-26 16:45:26 +0700

    preprocessv3

---
+++ buil_model.py
@@ -0,0 +1,3 @@
+import ténorflow as tf
+from tensorflow.keras import layers, models
+from tensorflow.keras.applications import MobileNetV2
--- training.py
+++
@@ -1,5 +0,0 @@
-import tensorflow as tf #thư viên dùng để dùng mô hình mobileNetV2
-import pandas as pd #Thư viện dùng để đọc file có cấu trúc
-import cv2 #dùng để đọc ảnh và xử lý ảnh
-import sklearn #thư viện dùng để chia dữ liệu thành tập train và test
-
